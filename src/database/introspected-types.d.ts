cb1dbeaaf022a514f603ed68cbffd79568586ad2
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-03-14 21:09:53 -0400

    add database/types stuff

---
+++ src/database/introspected-types.d.ts
@@ -0,0 +1,20 @@
+import type { ColumnType } from "kysely";
+
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+
+export interface Test {
+  id: number | null;
+  name: string | null;
+}
+
+export interface Users {
+  id: Generated<number>;
+  phone_number: string;
+}
+
+export interface DB {
+  test: Test;
+  users: Users;
+}
