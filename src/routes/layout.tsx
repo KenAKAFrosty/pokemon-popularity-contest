1bde53a33ad64d349dc152bae932566842610226
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-03-14 21:28:23 -0400

    cleanup unused

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,15 +1,8 @@
 import { component$, Slot } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
 
-
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
 
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
+
   return (
     <>
       <main>
