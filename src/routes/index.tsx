e3ade6d8712055de2f745d263ee15bf8e9c63379
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-03-14 21:27:02 -0400

    server code test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,21 @@
 import { component$ } from '@builder.io/qwik';
 import { DocumentHead, server$ } from '@builder.io/qwik-city';
-import { DatabaseTable } from '~/database/query-builder';
+import { getQueryBuilder } from '~/database/query-builder';
 
-const test = server$(()=> { 
-
+const getFirstUser = server$(async () => {
+  const qb = getQueryBuilder();
+  const user = await qb.selectFrom("users").selectAll().executeTakeFirst();
+  console.log(user);
 })
 
 export default component$(() => {
   return (<main>
     <h1>Pokémon Popularity Contest</h1>
+    <button
+      onClick$={() => {
+        getFirstUser();
+      }}
+    >Click me to run server code!</button>
   </main>);
 });
 
