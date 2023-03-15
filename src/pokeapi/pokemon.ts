0827e65bb5b52248d81900169bd644869f0eae2b
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2023-03-14 23:58:43 -0400

    loading pokemon in for logged in users now

---
+++ src/pokeapi/pokemon.ts
@@ -0,0 +1,33 @@
+const MIN_POKEMON_ID = 1;
+const MAX_POKEMON_ID = 1010;
+
+export function getRandomPokemonId() {
+    return Math.floor(Math.random() * (MAX_POKEMON_ID - MIN_POKEMON_ID) + MIN_POKEMON_ID);
+}
+
+interface Pokemon {
+    name: string;
+    id: number;
+    sprites: {
+        back_default: string | null
+        back_female: string | null
+        back_shiny: string | null
+        back_shiny_female: string | null
+        front_default: string | null
+        front_female: string | null
+        front_shiny: string | null
+        front_shiny_female: string | null
+    }
+}
+
+export async function getPokemon(id: number) {
+    if (id < MIN_POKEMON_ID || id > MAX_POKEMON_ID) {
+        return { success: false as const, error: "Invalid Pokemon ID" as const }
+    }
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const pokemon = await fetch(url).then(raw => raw.json());
+    return {
+        success: true as const,
+        pokemon: pokemon as Pokemon
+    }
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,11 @@ import { component$, useSignal } from '@builder.io/qwik';
 import { DocumentHead, routeLoader$, server$ } from '@builder.io/qwik-city';
 import { getQueryBuilder } from '~/database/query-builder';
 import { getUuId } from '~/generic-utilities';
+import { getPokemon, getRandomPokemonId } from '~/pokeapi/pokemon';
 import { checkToken, sendToken } from '~/twilio/verification';
 const testPhoneNumber = import.meta.env.VITE_TEST_PHONE_NUMBER;
 
-const useCurrentUser = routeLoader$(async (context) => {
+export const useCurrentUser = routeLoader$(async (context) => {
   const auth_token = context.cookie.get("auth_token");
   if (!auth_token) {
     return null
@@ -19,67 +20,72 @@ const useCurrentUser = routeLoader$(async (context) => {
   return user || null;
 });
 
-
-const frontendSendToken = server$(async (phoneNumber: string) => {
-  const phoneNumberToUse = phoneNumber === "test" ? testPhoneNumber : phoneNumber;
-  return sendToken(phoneNumberToUse);
-});
-
-const verifyUser = server$(async function (inputs: { code: string, phoneNumber: string }) {
-  const { phoneNumber, code } = inputs;
-  const phoneNumberToUse = phoneNumber === "test" ? testPhoneNumber : phoneNumber;
-  const result = await checkToken({
-    code,
-    phoneNumber: phoneNumberToUse
-  });
-  if (result.status === "approved") {
-    const token = getUuId();
-    const qb = getQueryBuilder();
-    const existingUser = await qb.selectFrom("users")
-      .selectAll()
-      .where("phone_number", "=", phoneNumberToUse)
-      .executeTakeFirst();
-    if (existingUser) {
-      await qb.updateTable("users")
-        .set({ auth_token: token })
-        .where("phone_number", "=", phoneNumberToUse)
-        .execute();
-    } else {
-      await qb.insertInto("users")
-        .values({
-          phone_number: phoneNumberToUse,
-          auth_token: token
-        })
-        .execute();
-    }
-
-    this.cookie.set("auth_token", token, {
-      maxAge: 60 * 60 * 24 * 30,
-      httpOnly: true,
-      path: "/",
-      secure: true
-    })
-    return { success: true as const }
+export const useTwoRandomPokemon = routeLoader$(async (context) => {
+  const user = await context.resolveValue(useCurrentUser);
+  if (!user) {
+    return null;
   } else {
-    return { success: false as const, message: "Code was not approved" as const }
+    const idOne = getRandomPokemonId();
+    const idTwo = getRandomPokemonId();
+    const pokemonResponses = await Promise.all([
+      getPokemon(idOne),
+      getPokemon(idTwo)
+    ]);
+    const pokemon = pokemonResponses.map(pokemonResponse => {
+      if (!pokemonResponse.success) {
+        throw new Error("Failed to get pokemon. Unexpected behavior")
+      }
+      return {
+        name: pokemonResponse.pokemon.name,
+        image: pokemonResponse.pokemon.sprites.front_default
+      }
+    });
+    return pokemon;
   }
-});
+
+})
 
 
 export default component$(() => {
   const currentUser = useCurrentUser();
+  const pokemon = useTwoRandomPokemon();
+  if (pokemon.value === null) {
+    throw new Error("Pokemon is null")
+  }
   console.log(currentUser.value)
 
   return (<main>
     <h1>Pokémon Popularity Contest</h1>
     {
       currentUser.value ?
-        <h2>Logged in. Raw user data {JSON.stringify(currentUser.value)}</h2>
+        <h2>Logged in.</h2>
         : <h2>Not logged in</h2>
     }
-    {!currentUser.value && <Login />}
+    {currentUser.value ? <PokemonPicker pokemon={pokemon.value} /> : <Login />}
   </main>);
 });
+
+
+
+export const PokemonPicker = component$((props: {
+  pokemon: { name: string, image: string | null }[]
+}) => {
+  const pokemonOneSignal = useSignal(props.pokemon[0]);
+  const pokemonTwoSignal = useSignal(props.pokemon[1]);
+  return <section>
+    <h2>Which Pokémon is more popular?</h2>
+    <div>
+      <img src={pokemonOneSignal.value.image || undefined} />
+      <p>{pokemonOneSignal.value.name}</p>
+    </div>
+    <div>
+      <img src={pokemonTwoSignal.value.image || undefined} />
+      <p>{pokemonTwoSignal.value.name}</p>
+    </div>
+  </section>
+})
+
+
 
 export const Login = component$(() => {
   const phoneNumberSignal = useSignal("");
@@ -120,7 +126,55 @@ export const Login = component$(() => {
       Verify
     </button>
   </section>
-})
+});
+
+
+
+export const frontendSendToken = server$(async (phoneNumber: string) => {
+  const phoneNumberToUse = phoneNumber === "test" ? testPhoneNumber : phoneNumber;
+  return sendToken(phoneNumberToUse);
+});
+
+export const verifyUser = server$(async function (inputs: { code: string, phoneNumber: string }) {
+  const { phoneNumber, code } = inputs;
+  const phoneNumberToUse = phoneNumber === "test" ? testPhoneNumber : phoneNumber;
+  const result = await checkToken({
+    code,
+    phoneNumber: phoneNumberToUse
+  });
+  if (result.status === "approved") {
+    const token = getUuId();
+    const qb = getQueryBuilder();
+    const existingUser = await qb.selectFrom("users")
+      .selectAll()
+      .where("phone_number", "=", phoneNumberToUse)
+      .executeTakeFirst();
+    if (existingUser) {
+      await qb.updateTable("users")
+        .set({ auth_token: token })
+        .where("phone_number", "=", phoneNumberToUse)
+        .execute();
+    } else {
+      await qb.insertInto("users")
+        .values({
+          phone_number: phoneNumberToUse,
+          auth_token: token
+        })
+        .execute();
+    }
+
+    this.cookie.set("auth_token", token, {
+      maxAge: 60 * 60 * 24 * 30,
+      httpOnly: true,
+      path: "/",
+      secure: true
+    })
+    return { success: true as const }
+  } else {
+    return { success: false as const, message: "Code was not approved" as const }
+  }
+});
+
 
 export const head: DocumentHead = {
   title: 'Pokémon Popularity Contest',
